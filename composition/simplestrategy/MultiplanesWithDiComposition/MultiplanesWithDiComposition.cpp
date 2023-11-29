cc3bba5f6a2b0e73f9dee88fc8013440709273a3
Tianhua Sun <tianhua.sun@amlogic.com>
2023-11-29 14:43:18 +0800

    hwc: don't disable VC when tunnel ID change [1/1]
    PD#SWPL-147035
    
    Problem:
    cannot keep last frame when tunnel ID change
    
    Solution:
    don't disable VC when tunnel ID change
    
    Verify:
    ohm
    
    Change-Id: I8b384b6830c630ab8c6bf4284b55beeade082454
    Signed-off-by: Tianhua Sun <tianhua.sun@amlogic.com>

--- composition/simplestrategy/MultiplanesWithDiComposition/MultiplanesWithDiComposition.cpp
+++ composition/simplestrategy/MultiplanesWithDiComposition/MultiplanesWithDiComposition.cpp
@@ -1531,7 +1531,7 @@ int MultiplanesWithDiComposition::commit() {
         uint32_t presentZorder = displayIt->presentZorder;
         std::shared_ptr<DrmFramebuffer> fb = displayIt->fb;
         std::shared_ptr<HwDisplayPlane> plane = displayIt->plane;
-        int blankFlag = (mHideSecureLayer && fb->mSecure) ?
+        drm_plane_blank_t blankFlag = (mHideSecureLayer && fb->mSecure) ?
             BLANK_FOR_SECURE_CONTENT : UNBLANK;
 
         if (composerOutput.get() &&
@@ -1552,12 +1552,6 @@ int MultiplanesWithDiComposition::commit() {
             bool bDumpPlane = true;
             bool bDoDiCompose = false;
             for (auto it = mDIComposerFbs.begin(); it != mDIComposerFbs.end(); ++it) {
-                if ((*it)->isVtNeedClearFrameOrShowColorBuffer()) {
-                    MESON_LOGV("%s, layerId(%" PRIu64 ") will blank plane", __func__, fb->mId);
-                    plane->setPlane(fb, presentZorder, BLANK_FOR_NO_CONTENT);
-                    continue;
-                }
-
                 if ((*it)->isVtBuffer())
                     bHaveVTBuffer = bDoDiCompose = true;
 
@@ -1579,26 +1573,19 @@ int MultiplanesWithDiComposition::commit() {
         if (fb->isVtBuffer()) {
             bHaveVTBuffer = true;
 
-            if (fb->isVtNeedClearFrameOrShowColorBuffer() ||
-                (fb->getBufferFd() < 0 && !fb->haveSolidColorBuffer())) {
-
+            if (fb->interruptVtProcess(blankFlag)) {
                 if (mVideoProcessorsMgr.get())
                     mVideoProcessorsMgr->resetProcessors(fb);
 
-                /* need blank video plane:
-                 * 1, received a clear last frame cmd
-                 * 2, buffer is invalid */
-                MESON_LOGV("%s, layerId(%" PRIu64 ") will blank plane", __func__, fb->mId);
-                plane->setPlane(fb, presentZorder, BLANK_FOR_NO_CONTENT);
-                continue;
-            }
-
-            if (fb->getBufferFd() < 0 && fb->haveSolidColorBuffer()) {
-                if (mVideoProcessorsMgr.get())
-                    mVideoProcessorsMgr->resetProcessors(fb);
+                if (blankFlag == BLANK_FOR_NO_CONTENT)
+                    MESON_LOGV("%s, layerId(%" PRIu64 ") will blank plane",
+                            __func__, fb->mId);
 
                 plane->setPlane(fb, presentZorder, blankFlag);
-                fb->freeSolidColorBuffer();
+
+                if (blankFlag != BLANK_FOR_NO_CONTENT)
+                    fb->freeSolidColorBuffer();
+
                 continue;
             }
         } else {
@@ -1725,7 +1712,7 @@ int MultiplanesWithDiComposition::commitTunnelVideo() {
         uint32_t presentZorder = displayIt->presentZorder;
         std::shared_ptr<DrmFramebuffer> fb = displayIt->fb;
         std::shared_ptr<HwDisplayPlane> plane = displayIt->plane;
-        int blankFlag = (mHideSecureLayer && fb->mSecure) ?
+        drm_plane_blank_t blankFlag = (mHideSecureLayer && fb->mSecure) ?
             BLANK_FOR_SECURE_CONTENT : UNBLANK;
 
         if (!fb->isVtBuffer() && fb->mCompositionType != MESON_COMPOSITION_DI)
@@ -1744,25 +1731,19 @@ int MultiplanesWithDiComposition::commitTunnelVideo() {
             continue;
         }
 
-        if (fb->isVtNeedClearFrameOrShowColorBuffer() ||
-            (fb->getBufferFd() < 0 && !fb->haveSolidColorBuffer())) {
+        if (fb->interruptVtProcess(blankFlag)) {
             if (mVideoProcessorsMgr.get())
                 mVideoProcessorsMgr->resetProcessors(fb);
 
-            /* need blank video plane:
-             * 1, received a clear last frame cmd
-             * 2, buffer is invalid */
-            MESON_LOGV("%s, layerId(%" PRIu64 ") will blank plane", __func__, fb->mId);
-            plane->setPlane(fb, presentZorder, BLANK_FOR_NO_CONTENT);
-            continue;
-        }
-
-        if (fb->getBufferFd() < 0 && fb->haveSolidColorBuffer()) {
-            if (mVideoProcessorsMgr.get())
-                mVideoProcessorsMgr->resetProcessors(fb);
+            if (blankFlag == BLANK_FOR_NO_CONTENT)
+                MESON_LOGV("%s, layerId(%" PRIu64 ") will blank plane",
+                        __func__, fb->mId);
 
             plane->setPlane(fb, presentZorder, blankFlag);
-            fb->freeSolidColorBuffer();
+
+            if (blankFlag != BLANK_FOR_NO_CONTENT)
+                fb->freeSolidColorBuffer();
+
             continue;
         }
 
